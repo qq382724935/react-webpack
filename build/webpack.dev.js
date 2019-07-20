36c7d785e357acb2e4d68a29cbac1c2795526f9c
qq382724935 <382724935@qq.com>
2019-07-20 14:34:25 +0800

    更新

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -2,17 +2,17 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common");
 const webpack = require("webpack");
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    devServer: {
-        hot: true,
-        open: true,
-        contentBase: "../dist",
-        compress: true,
-        // hotOnly:true 
-    },
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    hot: true,
+    open: true,
+    contentBase: "../dist",
+    compress: true,
+    // hotOnly:true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 });
