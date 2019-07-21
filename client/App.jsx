667983c2664deb9c79aa400a3580f4e0810bd2f1
qq382724935 <382724935@qq.com>
2019-07-21 22:59:26 +0800

    项目结构搭建

---
+++ client/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+import BasicLayout from "./layout/index";
+const App = () => <BasicLayout />;
+
+export default hot(App);
--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Main from "./main";
-ReactDom.render(<Main />, document.getElementById("root"));
+import App from "./App";
+ReactDom.render(<App />, document.getElementById("root"));
